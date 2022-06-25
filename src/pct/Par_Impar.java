ff98be40c90c3f616f39680d62f35d7412af6956
gilberto <gilberto@gilberto-C14CU51>
2022-06-25 11:49:24 -0300

    - Commit inicial: Projeto Par Impar.

---
+++ src/pct/Par_Impar.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pct;
+
+import java.util.Scanner;
+
+
+/**
+ *
+ * @author gilberto
+ */
+public class Par_Impar {
+    public static void main(String[] args) {
+        //Declaração de variáveis
+        int n;
+        
+        //Criação e instância do objeto de entrada
+        Scanner entrada = new Scanner(System.in);
+        
+        //Apresentação para o usuário
+        System.out.println("\n\t\t\t -- Par ou Impar --\n");
+        
+        //Entrada
+        System.out.print("\t Informe um número: ");
+        n = entrada.nextInt();
+        
+        //Processamento e Saída
+        if((n % 2) == 0){
+            System.out.println(n + " é par!\n");
+        }else{
+            System.out.println(n + " é impar!\n");
+        }
+        
+    }
+    
+}
